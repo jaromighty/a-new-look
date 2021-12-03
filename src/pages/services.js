d87d1549beec0ad59b9f12685886ac2212d0c177
Jarom Rial <jarom.rial@gmail.com>
2021-12-03 10:47:15 -0700

    add services page

---
+++ src/pages/services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppLayout from "../components/app-layout";
+import Header from "../components/header";
+
+export default function Services() {
+    return (
+        <AppLayout title="Services">
+            <div className="relative pt-6 pb-16 sm:pb-56">
+                <Header />
+            </div>
+
+            {/* FAQs */}
+        </AppLayout>
+    );
+}
