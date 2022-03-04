0e4ad1b5389dfee4555b48f323354b1954578351
Jarom Rial <jarom.rial@gmail.com>
2022-03-04 05:44:23 -0700

    update dummy text on home page

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
                 <div className="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
                     <h2 className="text-3xl font-extrabold text-white sm:text-4xl">Ready to see us at your door?</h2>
                     <p className="mt-4 text-lg leading-6 text-emerald-200">
-                        Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.
+                        We're excited to provide our quality service for your home or office!
                     </p>
                     <Link to="/contact" className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-emerald-600 bg-white hover:bg-emerald-50 sm:w-auto">Contact us today</Link>
                 </div>
