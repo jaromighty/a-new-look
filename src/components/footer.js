c5c1cb29a0c85d9189102954ef0f9df24d76c2fb
Jarom Rial <jarom.rial@gmail.com>
2022-03-04 05:51:25 -0700

    update footer alignment

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,12 +14,12 @@ export default function Footer() {
                     <Link to="/contact" className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-emerald-600 bg-white hover:bg-emerald-50 sm:w-auto">Contact us today</Link>
                 </div>
             </div>
-            <footer className="relative">
-                <div className="py-12 px-4 border-t border-gray-200 lg:flex lg:items-center lg:justify-between md:px-24 lg:px-40">
+            <footer className="relative max-w-7xl mx-auto">
+                <div className="py-12 px-4 border-t border-gray-200 xl:flex xl:items-center xl:justify-between">
                     <div className="text-center">
                         <p className="text-gray-500">Proudly serving Daytona Beach and surrounding areas</p>
                     </div>
-                    <div className="mt-12 flex justify-center md:mt-6 lg:justify-start lg:mt-0 md:order-2">
+                    <div className="mt-12 flex justify-center md:mt-6 xl:justify-start xl:mt-0 md:order-2">
                         <p className="inline-flex text-center text-base leading-6 text-gray-400">
                             Made with
                             <HeartIcon className="h-6 w-6 mx-1" />
@@ -27,7 +27,7 @@ export default function Footer() {
                             <a className="pl-1 underline md:no-underline md:hover:underline" href="https://jarom.dev/?ref=anewlook" target="_blank" rel="noopener noreferrer">Jarom Rial</a>
                         </p>
                     </div>
-                    <p className="mt-8 text-base text-center lg:text-left leading-6 text-gray-400 md:mt-6 lg:mt-0 md:order-1">
+                    <p className="mt-8 text-base text-center xl:text-left leading-6 text-gray-400 md:mt-6 xl:mt-0 md:order-1">
                         &copy; {new Date().getFullYear()} A New Look Restorations and Janitorial Service
                     </p>
                 </div>
