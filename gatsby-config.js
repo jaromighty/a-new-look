531e89ca750ed982e321c73a81402b4ef9bedbe9
Jarom Rial <jarom.rial@gmail.com>
2022-03-04 05:20:48 -0700

    update services page with content

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,20 @@ module.exports = {
     siteUrl: "https://anlrestorations.com",
     author: "",
     facebookPage: "https://www.facebook.com/ANewLookCarpetCleaning",
+    services: [
+      { 
+        name: "Carpet, furniture, tile, and floor cleaning", 
+        description: "We all know things get dirty. It’s a fact of life. Floors and furniture seem to often take pretty hard hits when it comes to cleanliness. Have you checked your couch for crumbs lately? Any type of material needs to be cleaned on a regular basis. Unfortunately, the longer you wait, the worse it gets. Dirt and grime work their way into that material over time and become harder to clean. Repetition is key to having clean floors and furniture. We recommend that your carpet, furniture, and tile be cleaned at least twice a year."
+      },
+      { 
+        name: "24-hour emergency water damage restoration",
+        description: "Emergency water damage can take different forms. You never know when it could happen. And you can only be so prepared. Here in Florida, these emergencies generally come in the form of hurricanes and tropical storms. Carpet, due to its location on the floor, gets saturated. When this happens, getting your carpets taken care of sooner will avoid having to replace them. Either the padding will need to be removed or the carpet simply dried. The type of damage determines which treatment is chosen. We aim to offer quick response times and make our restoration process as cost efficient as possible for you."
+      },
+      { 
+        name: "Complete home/office remodeling and floor installation",
+        description: "In the unfortunate event that more than your carpet endures damage, we also offer restorative services to get your home or office back into working condition. We began taking on more restoration services when we found that subcontractors were giving cheap service. Our goal is to help you as quickly as we can while giving you cost-efficient quality service."
+      },
+    ],
   },
   plugins: [
     'gatsby-plugin-postcss',
